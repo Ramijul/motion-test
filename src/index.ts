8276522333725644d450b516c17e1a181d0896db
rami <ramizul127@gmail.com>
2024-03-26 15:53:42 -0400

    exit if token is missing

--- src/index.ts
+++ src/index.ts
@@ -6,34 +6,34 @@ dotenv.config();
 const ACCESS_TOKEN = process.env.FB_TOKEN as string;
 const FB_API_VERSION = 'v19.0';
 
-if (!ACCESS_TOKEN?.length) {
-  console.log('Access Token is missing. Exiting program.');
-}
-
 async function fetchUserInfo(): Promise<Response> {
   return await fetch(`https://graph.facebook.com/${FB_API_VERSION}/me?fields=id,name,last_name&access_token=${ACCESS_TOKEN}`);
 }
 
-(async function () {
-  while (true) {
-    const resp: Response = await fetchUserInfo();
+if (!ACCESS_TOKEN?.length) {
+  console.log('Access Token is missing. Exiting program.');
+} else {
+  (async function () {
+    while (true) {
+      const resp: Response = await fetchUserInfo();
 
-    if (resp.status === 200) {
-      const userInfo: IUserInfo = await resp.json();
-      if (!userInfo) {
-        console.log('No response. Please make sure token provided is correct');
+      if (resp.status === 200) {
+        const userInfo: IUserInfo = await resp.json();
+        if (!userInfo) {
+          console.log('No response. Please make sure token provided is correct');
+          return;
+        }
+        console.log(userInfo);
+      }
+
+      const backoff: number | null = await getBackoff(resp);
+
+      if (!backoff) {
+        //backoff returned is null, exit the program
         return;
       }
-      console.log(userInfo);
+      // wait
+      await sleep(backoff);
     }
-
-    const backoff: number | null = await getBackoff(resp);
-
-    if (!backoff) {
-      //backoff returned is null, exit the program
-      return;
-    }
-    // wait
-    await sleep(backoff);
-  }
-})();
+  })();
+}
