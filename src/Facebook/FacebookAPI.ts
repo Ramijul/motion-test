b73aea0f5789445579302d13352b73c8fda363f5
rami <ramizul127@gmail.com>
2024-03-30 09:49:03 -0400

    allow injecting requestHandler

--- src/Facebook/FacebookAPI.ts
+++ src/Facebook/FacebookAPI.ts
@@ -4,13 +4,15 @@ import { IUserInfo } from './types';
 export class FacebookAPI {
   private FB_API_VERSION = 'v19.0';
   private ACCESS_TOKEN: string;
+  private requestHandler: typeof RequestHandler;
 
-  constructor() {
+  constructor(requesthandler: typeof RequestHandler = RequestHandler) {
     this.ACCESS_TOKEN = process.env.FB_TOKEN as string;
 
     if (!this.ACCESS_TOKEN?.length) {
       throw new Error('Access Token is missing');
     }
+    this.requestHandler = requesthandler;
   }
 
   /**
@@ -18,8 +20,8 @@ export class FacebookAPI {
    * @param fields
    * @returns
    */
-  public async fetchUserInfo(fields: string[]): Promise<IUserInfo> {
+  async fetchUserInfo(fields: string[]): Promise<IUserInfo> {
     const endpoint = `https://graph.facebook.com/${this.FB_API_VERSION}/me?fields=${fields.toString()}&access_token=${this.ACCESS_TOKEN}`;
-    return (await RequestHandler.fetchData(endpoint)) as IUserInfo;
+    return (await this.requestHandler.fetchData(endpoint)) as IUserInfo;
   }
 }
